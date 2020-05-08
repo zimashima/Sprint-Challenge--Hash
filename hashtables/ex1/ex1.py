6e964e38951e413ba5e355af3a8e3f7c432d7a07
Mashima <52222646+zimashima@users.noreply.github.com>
2020-05-08 12:52:43 -0400

    MVP + 1 stretch

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,19 @@
 def get_indices_of_item_weights(weights, length, limit):
 
-    """
-    YOUR CODE HERE
-    """
+    hash_weight = {}
 
+    for ind, i in enumerate(weights):
+        if i in hash_weight:
+            hash_weight[i].append(ind)
+        else:
+            hash_weight[i] = [ind]
+
+    for i in hash_weight:
+        diff = limit - i
+        if (diff in hash_weight):
+            if len(hash_weight[diff]) > 1:
+                return [hash_weight[diff][1], hash_weight[diff][0]]
+            else:
+                return [hash_weight[diff][0], hash_weight[i][0]]
+            
     return None
